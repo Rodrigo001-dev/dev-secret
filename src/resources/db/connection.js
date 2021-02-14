8c16c34f008a62a7fde1997bf04db5509839b48b
Rodrigo <rodrigorael53@gmail.com>
2021-02-14 19:42:32 -0300

    connecting to the database

---
+++ src/resources/db/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+let connection = null;
+
+const URI = process.env.MONGO_URI;
+
+module.exports = async () => {
+  if (!connection) {
+    connection = mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+
+    await connection;
+  }
+};
