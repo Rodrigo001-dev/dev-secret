c6be942260bb4de6a444800c64dba7d97373b5ca
Rodrigo <rodrigorael53@gmail.com>
2021-02-15 11:16:59 -0300

    adding logic of creating a secret friend route

--- src/handlers/secrets.js
+++ src/handlers/secrets.js
@@ -1,5 +1,30 @@
+require('../resources/db/connection')();
+
+const SecretModel = require('../resources/db/models/Secret');
+
 module.exports.create = async (event) => {
-  
+  // dentro do event vai ter várias informações como por exemplo: 
+  // o body, headers entre outras informações
+
+  // o event.body vem como String e como vai ser enviado um json para a API
+  // precisamos dar um JSON.parse
+  const { name, email } = JSON.parse(event.body)
+
+  try {
+    SecretModel.create({
+      owner: name,
+      ownerEmail: email,
+      externalId: '',
+      adminKey: '',
+    });
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false
+      }),
+    }
+  }
 };
 
 module.exports.get = async (event) => {
@@ -7,5 +32,5 @@ module.exports.get = async (event) => {
 };
 
 module.exports.draw = async (event) => {
-  
+
 };
