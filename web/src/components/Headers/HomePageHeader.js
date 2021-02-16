b743e87a9272f8732a581461bc18861876545423
Rodrigo <rodrigorael53@gmail.com>
2021-02-16 20:45:08 -0300

    update Header

--- web/src/components/Headers/HomePageHeader.js
+++ web/src/components/Headers/HomePageHeader.js
@@ -1,9 +1,39 @@
+import styled from 'styled-components';
+
 import ImageContainer from '../Comtainers/ImageContainer';
+import Logo from '../Logo';
+import NameEmailForm from '../NameEmailForm';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  min-height: 80vh;
+
+  color: ${({ theme }) => theme.colors.light};
+  padding-left: 40px;
+`;
+
+const FormDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+`;
 
 export default function HomePageHeader () {
   return (
     <ImageContainer>
-      HomePageHeader
+      <Container>
+        <Logo />
+        <h2>A melhor brincadeira do natal.</h2>
+      </Container>
+      <FormDiv>
+        <NameEmailForm />
+      </FormDiv>
     </ImageContainer>
   );
 };
